e6d695c65588e9c411bc94184bab833f4e63dfa9
Viktor09890 <103953144+Viktor09890@users.noreply.github.com>
2022-04-18 19:37:34 +0300

    Create fdgvdvfb.js

---
+++ fdgvdvfb.js
@@ -0,0 +1,9 @@
+/* eslint no-use-before-define: 0 */
+import Frame from '../src/Frame';
+import Game from '../src/Game';
+
+
+describe('Demo Test Suite', () => {
+
+  test('Demo test', () => {
+        expect(1).toBe(1);
